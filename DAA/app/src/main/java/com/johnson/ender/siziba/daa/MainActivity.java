628a70be78a66c628a7f53b7c49b3df173711e12
Johnson Andrew Siziba <siziba.internet@gmail.com>
2020-09-09 14:12:49 +0200

    Create Android Apps

---
+++ DAA/app/src/main/java/com/johnson/ender/siziba/daa/MainActivity.java
@@ -0,0 +1,14 @@
+package com.johnson.ender.siziba.daa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
